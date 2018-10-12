82757be203600d033be6cde35c28e2aae00e6831
Steve James <steve.james@isentia.com>
2018-10-12 15:38:58 +1100

    Updated Querying Interface

--- src/IObjectStoreQueryResult.ts
+++ src/IObjectStoreQueryResult.ts
@@ -1,8 +1,14 @@
 import { IModel } from 'that-simple-model'
 
+export enum ObjectStoreQueryStatus {
+	OK = 'OK',
+	ERROR = 'ERROR'
+}
+
 /**
  * An object type describing Query Results
  */
 export interface IObjectStoreQueryResult<T extends IModel> {
+	status: ObjectStoreQueryStatus
 	items: Array<T>
 }
